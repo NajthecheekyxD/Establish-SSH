05c95484bdad809145ff27686371131e62b46a98
NajthecheekyxD <146105592+NajthecheekyxD@users.noreply.github.com>
2023-10-17 16:55:03 +0100

    Add files via upload

--- establish-a-ssh-connection.py
+++ establish-a-ssh-connection.py
@@ -4,8 +4,8 @@ def connect_to_ssh(ip_address, username, password):
     ssh = paramiko.SSHClient()
     ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
     ssh.connect(ip_address, username=username, password=password)  
-connect_to_ssh("192.168.1.1", "username", "password")
-
+connect_to_ssh("192.168.56.101", "username", "password")
+print('8')
 
 # Define the SSH connection parameters
 ip_address = '192.168.56.101'
@@ -31,7 +31,7 @@ except paramiko.SSHException as sshException:
 except paramiko.BadHostKeyException as badHostKeyException:
     print('---FAILURE! Unable to verify server\'s host key: ', badHostKeyException)
     exit()
-
+print('34')
 # If the connection was successful, create a new channel for remote commands
 channel = ssh.invoke_shell()
 
@@ -39,15 +39,15 @@ channel = ssh.invoke_shell()
 channel.send('configure terminal\n')
 channel.send('hostname ' + new_hostname + '\n')
 channel.send('end\n')
-
+print("11")
 # Wait for the command to complete
 while not channel.recv_ready():
     pass
-
+print("24")
 # Print the output of the command
 print(channel.recv(1024).decode('utf-8'))
-
+print("36")
 # Close the SSH connection
 ssh.close()
-
+print("43")
 
